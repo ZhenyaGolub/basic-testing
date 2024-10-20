bf3f9c7b7fd854b2226df9ea01d6c117960685ee
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-20 22:15:18 +0300

    feat: add tests for 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,24 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const axiosClient = axios.create({
+      baseURL,
+    });
+    expect(axiosClient.getUri()).toBe(baseURL);
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    throttledGetDataFromApi('/posts/1')?.then((response) =>
+      expect(response.config.url).toBe('/posts/1'),
+    );
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const response = await throttledGetDataFromApi('/posts/1');
+    expect(response.id).toBe(1);
   });
 });
